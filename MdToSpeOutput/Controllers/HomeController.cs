614f346226bdf96f87317e12008d1585317b5622
RiyaRai01 <riyarai.safali@gmail.com>
2024-04-22 19:43:28 +0530

    MVC Markdown Transformer

---
+++ MdToSpeOutput/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MdToSpeOutput.Models;
+using Markdig;
+
+namespace MdToSpeOutput.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Upload()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Upload(IFormFile markdownFile)
+    {
+        if(markdownFile == null || markdownFile.Length == 0)
+        {
+            ViewBag.Error = "Please Select the correct file to upload";
+            return View();
+        }
+
+        using(var reader = new StreamReader(markdownFile.OpenReadStream()))
+        {
+            string markdownContent = reader.ReadToEnd();
+            string htmlContent = TransformMarkdownToHtml(markdownContent);
+            System.Console.WriteLine(htmlContent);
+            ViewBag.htmlContent = htmlContent;
+        }
+        return View("TransformContent");
+    }
+
+    public string TransformMarkdownToHtml(string markdownContent)
+    {
+         // Configure the Markdown pipeline
+        var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+ 
+        // // Convert Markdown to HTML
+        // string html = Markdig.Markdown.ToHtml(markdownText, pipeline);
+        var mdToHtml = Markdig.Markdown.ToHtml(markdownContent,pipeline);
+        return mdToHtml;
+    }
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ MdToSpeOutput/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MdToSpeOutput.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
